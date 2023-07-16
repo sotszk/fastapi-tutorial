600fc86b74cf0bb1e0c0c7fc4de883c24999e8cb
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-07-16 22:00:16 +0900

    Read a section of Query parameter

---
+++ db/fake_items.py
@@ -0,0 +1,5 @@
+fake_items_db = [
+    {"item_name": "Foo"},
+    {"item_name": "Bar"},
+    {"item_name": "Baz"},
+]
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+from typing import Union
 from fastapi import FastAPI
 
 from modules import models
+from db import fake_items
 
 app = FastAPI()
 
@@ -12,11 +14,21 @@ async def root():
     }
 
 
+@app.get("/items")
+async def read_items(skip: int = 0, limit: int = 10):
+    return fake_items.fake_items_db[skip: skip + limit]
+
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
-    return {
-        "item_id": item_id,
-    }
+async def read_item(item_id: int, q: Union[str, None] = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:  # ?short={True|true|1|on|yes}
+        item.update({
+            "description": "This is an amazing super item that has a long description"
+        })
+    return item
 
 
 @app.get("/users/me")
@@ -32,6 +44,15 @@ async def read_user(user_id: str):
     return {
         "user_id": user_id,
         "name": "fakeuser"
+    }
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(user_id: str, item_id: int, q: Union[str, None] = None):
+    return {
+        "user_id": user_id,
+        "item_id": item_id,
+        "q": q
     }
 
 
