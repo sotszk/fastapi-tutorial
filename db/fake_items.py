81d5bc3b771d5ee1913d8de2052f1f7db8b1dce6
SotaSuzuki <3.3sota.suzuki@gmail.com>
2023-07-30 00:11:09 +0900

    apply black formatter

--- db/fake_items.py
+++ db/fake_items.py
@@ -2,4 +2,9 @@ fake_items_db = [
     {"item_name": "Foo"},
     {"item_name": "Bar"},
     {"item_name": "Baz"},
+    {"item_name": "Jin"},
+    {"item_name": "Hoo"},
+    {"item_name": "Eee"},
+    {"item_name": "Que"},
+    {"item_name": "Zee"},
 ]
--- main.py
+++ main.py
@@ -9,9 +9,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {
-        "message": "Hello World!"
-    }
+    return {"message": "Hello World!"}
 
 
 @app.get("/items")
@@ -25,35 +23,29 @@ async def read_item(item_id: int, q: Union[str, None] = None, short: bool = Fals
     if q:
         item.update({"q": q})
     if not short:  # ?short={True|true|1|on|yes}
-        item.update({
-            "description": "This is an amazing super item that has a long description"
-        })
+        item.update(
+            {"description": "This is an amazing super item that has a long description"}
+        )
     return item
 
 
 @app.get("/users/me")
 async def read_user_me():
-    return {
-        "user_id": "the current user",
-        "name": "fakecurrentuser"
-    }
+    return {"user_id": "the current user", "name": "fakecurrentuser"}
 
 
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
-    return {
-        "user_id": user_id,
-        "name": "fakeuser"
-    }
+    return {"user_id": user_id, "name": "fakeuser"}
 
 
 @app.get("/users/{user_id}/items/{item_id}")
 async def read_user_item(user_id: str, item_id: int, q: Union[str, None] = None):
-    return {
-        "user_id": user_id,
-        "item_id": item_id,
-        "q": q
-    }
+    item = {"user_id": user_id, "item_id": item_id}
+    if q:
+        item.update({"q": q})
+
+    return item
 
 
 @app.get("/models/{model_name}")
@@ -63,6 +55,4 @@ async def get_model(model_name: models.ModelName):
 
 @app.get("/files/{file_path:path}")
 async def read_file(file_path: str):
-    return {
-        "file_path": file_path
-    }
+    return {"file_path": file_path}
